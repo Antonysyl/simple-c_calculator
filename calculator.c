45c6ef4d9dd61a1001fad70561a62bda1cd42125
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-16 11:07:11 +0100

    simplified calculator

--- calculator.c
+++ calculator.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <math.h>
 
+/**
+ * This calculator was created by me at a beginners level of C programming language
+ * added basic operators in this program
+ * if you are reading this and you want to create your first simplifed c calculator you are free to use this program
+ * go through it, add some other operators used in calculators e.t.c
+ */
 
 int main(int argc, char *argv[])
 {
